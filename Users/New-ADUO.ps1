7d6992d12af0acee2ba1eb957019f3f588917d67
Piarre <pierre@ideestore.fr>
2023-02-17 13:08:34 +0100

    Added script for UO create.

---
+++ Users/New-ADUO.ps1
@@ -0,0 +1,24 @@
+﻿Clear-Host
+$yes = "Y"
+$domain = (Get-ADDomain).distinguishedName
+
+$groupName = Read-Host "Quel est le nom de l'UO en cours de création ? "
+
+if ( (Read-Host "Créer cette UO à la racine ? Y/N") -like $yes ) {
+    if (Get-ADOrganizationalUnit -Filter "distinguishedName -eq 'OU=$groupName,$domain'") { 
+        Write-Host "[ERREUR] Une UO existe déjà avec ce nom, création impossible !"
+        Exit 
+    }
+    New-ADOrganizationalUnit -Name $groupName
+    Write-Host
+    Get-ADOrganizationalUnit -Filter "distinguishedName -eq 'OU=$groupName,$domain'"
+}
+else {
+    Write-Output (Get-ADOrganizationalUnit -Filter 'Name -like "*"') | Format-Table Name, DistinguishedName -A
+    $whereUOCreation = Read-Host "Parmis vos UOs, dans quel UO créer celle-ci ? (nom de l'UO seulement) "
+    if (Get-ADOrganizationalUnit -Filter "distinguishedName -eq 'OU=$groupName,OU=$whereUOCreation                                                                                                                                                           ,$domain'") { 
+        Write-Host "[ERREUR] Une UO existe déjà avec ce nom, création impossible !"
+        Exit 
+    }
+    New-ADOrganizationalUnit -Name $groupName -Path "OU=$whereUOCreation,$domain"
+}
